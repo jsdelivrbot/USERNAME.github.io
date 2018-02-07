6ad83e1d133f62670f4280014d0a4baf4b579d78
Guillermo Castro <gcd20045@gmail.com>
2018-02-07 14:51:50 +0000

    Add save keybinding to geometric_pattern

--- geometric_pattern/main.js
+++ geometric_pattern/main.js
@@ -1,6 +1,9 @@
 var NUM_Circles = 12;
 var circleDiameter;
 var circleRadius;
+var rVal;
+var gVal;
+var bVal;
 
 function setup() {
     createCanvas(480, 600);
@@ -9,6 +12,10 @@ function setup() {
 }
 
 function draw() {
+    rVal = 255;
+    gVal = 0;
+    bVal = 0;
+    
    var isShifted = false; 
    
    var y = height;
@@ -21,7 +28,8 @@ function draw() {
    } else {
        x = 0;
    }
-   stroke(color(0, 255, 0));
+   fill(color(rVal,gVal,bVal))
+   stroke(color(rVal,gVal,bVal));
    while(x<= width){
      ellipse(x, y, circleDiameter, circleDiameter);
      x = x + circleDiameter;  
@@ -29,5 +37,16 @@ function draw() {
    
    y = y - circleRadius;
    isShifted = !isShifted;
+   
+   rVal = rVal - 2;
+   gVal = gVal + 7;
+   bVal = bVal + 3;
   }
+}
+
+function keyPressed() {
+    if(keyCode === 115 || keyCode === 83) {
+       saveCanvas('geometric pattern', 'png');
+    }
+    return false;
 }
